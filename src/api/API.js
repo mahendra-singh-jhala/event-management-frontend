ca98084b0f375f693d5d22971a5f5c47da7e9359
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-07-11 09:48:49 +0530

    update in api

---
+++ src/api/API.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const API_BASE_URL = "http://localhost:5000"
+
+const api = axios.create({
+    baseURL: API_BASE_URL
+})
+
+api.interceptors.request.use((config) => {
+    const user = JSON.parse(localStorage.getItem("auth"));
+    if (user?.token) {
+        config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+}, 
+    (error) => Promise.reject(error)
+)
+
+export default api
